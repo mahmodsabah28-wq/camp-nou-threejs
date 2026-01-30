0780f5a4ba78919ef12ab79ff0e3ba1e921f0480
mahmodsabah28-wq <mahmodsabah28@gmail.com>
2026-01-30 23:31:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+document.body.style.background = "red";
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
 
